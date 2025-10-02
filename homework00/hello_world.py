5f3f48eff4d976fc91d53e32b3c945ca77160317
Choiros-777 <ekaterinaromanova0077@gmail.com>
2025-10-02 14:02:23 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,2 +1,5 @@
-def text():
+''' This is a function for printing out the message hello world'''
+def text(message):
+    '''this function uses a variable called message'''
+    message = "hello world!"
     return message
