f0d807374fd415dbe0dfb7443c7ac9979163d7c9
Choiros-777 <ekaterinaromanova0077@gmail.com>
2025-10-02 14:37:53 +0300

    Описание исправлений

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,5 +1,7 @@
-"""  This is a function for printing out the message hello world """
+"""This is a function for printing out the message hello world"""
+
+
 def text(message):
-    """ this function uses a variable called message"""
+    """this function uses a variable called message"""
     message = "hello world!"
     return message
