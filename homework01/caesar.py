4bdbb5b2f2e728eec51c1587e336e5ac446c2619
Choiros-777 <ekaterinaromanova0077@gmail.com>
2025-11-03 19:42:54 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,7 +11,28 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    arr =[]
+    for char in plaintext:
+        if char.isupper() == True:
+            if (90 - ord(char)) < shift:
+                if shift > 26:
+                    a = shift - shift % 26
+                a = abs(90 - (ord(char) + shift))
+                arr.append(chr(65 + a - 1))
+            else:
+                arr.append(chr(ord(char) + shift))
+
+        elif char.islower() ==True:
+            if (122 - ord(char)) < shift:
+                if shift > 26:
+                    a = shift - shift % 26
+                a = abs(122 - (ord(char) + shift))
+                arr.append(chr(97 + a - 1))
+            else:
+                arr.append(chr(ord(char) + shift))
+
+        else: arr.append(char)
+    ciphertext = ''.join(arr)
     return ciphertext
 
 
@@ -28,5 +49,26 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    arr =[]
+    for char in ciphertext:
+        if 65<= ord(char) <=90:
+            if abs(65 - ord(char)) < shift:
+                if shift > 26:
+                    a = shift - shift % 26
+                a = abs(65 - (ord(char) - shift))
+                arr.append(chr(90 - a + 1))
+            else:
+                arr.append(chr(ord(char) - shift))
+
+        elif 97 <= ord(char) <=122:
+                if abs(97 - ord(char)) < shift:
+                    if shift > 26:
+                        a = shift - shift % 26
+                    a = abs(97 - (ord(char) - shift))
+                    arr.append(chr(122 - a + 1))
+                else:
+                    arr.append(chr(ord(char) - shift))
+        else:
+            arr.append(char)
+    plaintext = ''.join(arr)
     return plaintext
