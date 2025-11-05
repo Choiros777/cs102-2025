9d4df3b1b8acde167dae33668a6ce051f3e1e8c6
Choiros-777 <ekaterinaromanova0077@gmail.com>
2025-11-05 12:43:30 +0300

    i did it

---
+++ homework01/adbash.py
@@ -0,0 +1,28 @@
+
+def encrypt_atbash(plaintext):
+    ciphertext = ''
+    arr = []
+    i=0
+    for char in plaintext:
+
+        if char.isupper() == True: #90-65
+            charnumber = int(26 - (ord(char) - 64) + 1)
+            arr.append(chr(64+charnumber))
+
+        elif char.islower() == True: #122-97
+            charnumber = int(26 - (ord(char) - 96) + 1)
+            arr.append(chr(96 + charnumber))
+        else:
+            arr.append(char)
+            i += 1
+
+    ciphertext = "".join(arr)
+
+    return ciphertext
+
+
+print(encrypt_atbash("zzzaaa"))
+print(encrypt_atbash("NOON"))
+print(encrypt_atbash("Mom"))
+print(encrypt_atbash("MoM!!"))
+print(encrypt_atbash("Gr8"))
